7695e74dc0368292d38fe1086b104fab05f93c1b
H-a-r-p-r-e-e-t <baljeetakkashyap@gmail.com>
2026-06-28 23:10:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,85 +13,51 @@ const next = document.getElementById("next");
 let current = 0;
 
 function showImage(index) {
-
     current = index;
-
     lightboxImage.src = images[current].src;
-
     lightbox.classList.add("active");
-
 }
 
 images.forEach((img, index) => {
-
     img.addEventListener("click", () => {
-
         showImage(index);
-
     });
-
 });
 
 close.onclick = () => {
-
     lightbox.classList.remove("active");
-
 }
 
 lightbox.onclick = (e) => {
-
     if (e.target === lightbox)
-
         lightbox.classList.remove("active");
-
 }
 
 next.onclick = (e) => {
-
     e.stopPropagation();
-
     current++;
-
     if (current >= images.length)
-
         current = 0;
-
     showImage(current);
-
 }
 
 prev.onclick = (e) => {
-
     e.stopPropagation();
-
     current--;
-
     if (current < 0)
-
         current = images.length - 1;
-
     showImage(current);
-
 }
 
 document.addEventListener("keydown", (e) => {
-
     if (!lightbox.classList.contains("active"))
-
         return;
-
     if (e.key === "Escape")
-
         lightbox.classList.remove("active");
-
     if (e.key === "ArrowRight")
-
         next.click();
-
     if (e.key === "ArrowLeft")
-
         prev.click();
-
 });
 
 // hamburger
@@ -99,27 +65,13 @@ document.addEventListener("keydown", (e) => {
 const menuToggle=document.getElementById("menu-toggle");
 const navLinks=document.getElementById("nav-links");
 
-menuToggle.addEventListener("click",()=>{
-
-    navLinks.classList.toggle("active");
-
-});
-
 document.querySelectorAll("#nav-links a").forEach(link=>{
-
     link.addEventListener("click",()=>{
-
         navLinks.classList.remove("active");
-
     });
-
 });
 
 menuToggle.addEventListener("click",()=>{
-
     navLinks.classList.toggle("active");
-
-    menuToggle.textContent =
-        navLinks.classList.contains("active") ? "✕" : "☰";
-
+    menuToggle.textContent = navLinks.classList.contains("active") ? "✕" : "☰";
 });
